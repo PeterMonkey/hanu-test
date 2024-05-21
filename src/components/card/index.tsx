134d593a842d5dbbaeb1f2581df0a7e0beec87aa
Pedro Fernandez <pefernandez1408@gmail.com>
2024-05-21 14:17:15 -0400

    ADD: Se crearon los estilos para las cartas

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,12 +7,39 @@ type PropsCard = {
 }
 
 const Card = ({numb, suit}: PropsCard) => {
+
+const numberOfCard = [];
+if(numb <= 10) {
+    for (let i = 0; i < numb; i++) {
+      numberOfCard.push(<span key={i}>{suit}</span>);
+    }
+} else if (numb > 10 || numb <= 13) {
+    switch (numb) {
+        case 11:
+            numberOfCard.push(<span key={1}>{suit}</span>)
+            break;
+        case 12:
+            numberOfCard.push(<span key={1} >{suit}</span>)
+            break;
+        case 13:
+            numberOfCard.push(<span key={1} >{suit}</span>)
+            break;
+        default:
+            break;
+    }
+}
+
     return (
-        <div className={styles.card}>
+        <div className={styles.card} data-suit={suit} data-value={
+            numb <= 10 ? numb :
+            numb === 11 ? 'J' :
+            numb === 12 ? 'Q' :
+            numb === 13 ? 'K' : null
+        }
+            style={(suit === Suit['heart'] || suit === Suit['diamond']) ? {color: 'red'} : {}}>
             <div className={styles[`card-${numb}`]} 
-            style={(suit === Suit['heart'] || suit === Suit['diamond']) ? {color: 'red'} : {}}
             >
-                <span>{suit}</span>
+               {numberOfCard}
             </div>
         </div>
     )
--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -16,6 +16,90 @@ type Story = StoryObj<typeof meta>;
 export const A: Story = {
     args: {
         numb: 1,
-        suit: Suit['spade']
+        suit: Suit['heart']
+    }
+}
+
+export const Two: Story = {
+    args: {
+        numb: 2,
+        suit: Suit['heart']
+    }
+}
+
+export const Three: Story = {
+    args: {
+        numb: 3,
+        suit: Suit['heart']
+    }
+}
+
+export const Four: Story = {
+    args: {
+        numb: 4,
+        suit: Suit['heart']
+    }
+}
+
+export const Five: Story = {
+    args: {
+        numb: 5,
+        suit: Suit['heart']
+    }
+}
+
+export const Six: Story = {
+    args: {
+        numb: 6,
+        suit: Suit['heart']
+    }
+}
+
+export const Seven: Story = {
+    args: {
+        numb: 7,
+        suit: Suit['heart']
+    }
+}
+
+export const Eight: Story = {
+    args: {
+        numb: 8,
+        suit: Suit['heart']
+    }
+}
+
+export const Nine: Story = {
+    args: {
+        numb: 9,
+        suit: Suit['heart']
+    }
+}
+
+export const Ten: Story = {
+    args: {
+        numb: 10,
+        suit: Suit['heart']
+    }
+}
+
+export const J: Story = {
+    args: {
+        numb: 11,
+        suit: Suit['heart']
+    }
+}
+
+export const Q: Story = {
+    args: {
+        numb: 12,
+        suit: Suit['heart']
+    }
+}
+
+export const K: Story = {
+    args: {
+        numb: 13,
+        suit: Suit['heart']
     }
 }
